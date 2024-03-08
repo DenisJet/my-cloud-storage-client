7138b6907d0a2a34dd7112721abd95e15d8e9f90
Denis Kalkopf <deniskalkopf@gmail.com>
2024-03-09 00:02:11 +0500

    fix

--- src/actions/auth.actions.ts
+++ src/actions/auth.actions.ts
@@ -1,7 +1,6 @@
 'use server';
 import { cookies } from 'next/headers';
 import { LoginFormDTO, RegisterFormDTO } from '../dto/auth.dto';
-import { redirect } from 'next/navigation';
 
 ////////////////////////////////////////////////////
 
