b7b46d7e2d9746b25fc2d9058fc14b807e170220
Denis Kalkopf <deniskalkopf@gmail.com>
2024-03-09 19:36:54 +0500

    add activeCard state

--- src/components/FileCard/FileCard.tsx
+++ src/components/FileCard/FileCard.tsx
@@ -1,5 +1,7 @@
+'use client';
 import { getExt } from '@/utils/getExt';
 import { isImage } from '@/utils/isImage';
+import { useEffect, useRef, useState } from 'react';
 
 interface FileCardProps {
   filename: string;
@@ -7,17 +9,36 @@ interface FileCardProps {
 }
 
 export const FileCard: React.FC<FileCardProps> = ({ originalName, filename }) => {
+  const [activeCard, setActiveCard] = useState<boolean>(false);
+  const cardRef = useRef<HTMLDivElement>(null);
+
   const ext = getExt(filename);
   const imageUrl = ext && isImage(ext) ? 'http://localhost:7777/uploads/' + filename : '';
 
+  const clickHandler = (evt) => {
+    if (cardRef.current && !cardRef.current.contains(evt.target)) {
+      setActiveCard(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', clickHandler);
+  }, []);
+
   return (
-    <div className='w-20 align-self: stretch truncate cursor-pointer hover:opacity-75 text-center'>
+    <div
+      onClick={() => setActiveCard(!activeCard)}
+      ref={cardRef}
+      className='w-20 align-self: stretch truncate cursor-pointer hover:opacity-75 text-center relative'
+      title={originalName}
+    >
       {ext && isImage(ext) ? (
         <img className='w-20 h-20 object-contain' src={imageUrl} alt='image' />
       ) : (
         <img className='w-20 h-20 object-contain' src='/icon-text-file-80.png' alt='image' />
       )}
       <span className='text-xs'>{originalName}</span>
+      {activeCard && <div className='absolute top-0 left-0'>Buttons</div>}
     </div>
   );
 };
--- src/components/FileList/FileList.tsx
+++ src/components/FileList/FileList.tsx
@@ -1,16 +1,11 @@
-'use client';
-
 import { FileItem } from '@/dto/file.dto';
 import { FileCard } from '../FileCard/FileCard';
-import { useState } from 'react';
 
 interface FileListProps {
   files: FileItem[];
 }
 
 export const FileList: React.FC<FileListProps> = ({ files }) => {
-  const [activeCard, setActiveCard] = useState();
-
   return (
     <ul className='flex gap-2 items-center flex-wrap'>
       {files &&
