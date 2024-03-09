e9db56c1fcd998cd5e51616c64f2aa03d85fe4cd
Denis Kalkopf <deniskalkopf@gmail.com>
2024-03-09 18:57:21 +0500

    fix

--- src/components/FileList/FileList.tsx
+++ src/components/FileList/FileList.tsx
@@ -1,12 +1,21 @@
+'use client';
+
 import { FileItem } from '@/dto/file.dto';
 import { FileCard } from '../FileCard/FileCard';
+import { useState } from 'react';
 
-export const FileList = ({ files }) => {
+interface FileListProps {
+  files: FileItem[];
+}
+
+export const FileList: React.FC<FileListProps> = ({ files }) => {
+  const [activeCard, setActiveCard] = useState();
+
   return (
     <ul className='flex gap-2 items-center flex-wrap'>
       {files &&
         files.map((file: FileItem) => (
-          <li>
+          <li key={file.id}>
             <FileCard filename={file.filename} originalName={file.originalName} />
           </li>
         ))}
