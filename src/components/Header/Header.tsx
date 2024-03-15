6067812419857f980f89352fbf47f9572810ce13
Denis Kalkopf <deniskalkopf@gmail.com>
2024-03-15 11:36:34 +0500

    fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,5 +3,9 @@
 import styles from './header.module.css';
 
 export const Header = () => {
-  return <header className={styles.header}>My Cloud</header>;
+  return (
+    <header className={styles.header}>
+      <div className='max-w-screen-xl m-auto'>My Cloud</div>
+    </header>
+  );
 };
